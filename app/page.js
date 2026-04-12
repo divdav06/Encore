3c8da988f15e4d31ce442b753423b4a7bdc8fc97
divdav06 <moustapha.khalifaa@gmail.com>
2026-04-12 19:31:23 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main style={{padding: 20}}>
+      <h1>Selma App ⚽</h1>
+      <p>Bienvenue sur ton site de statistiques sportives</p>
+    </main>
+  );
+}
