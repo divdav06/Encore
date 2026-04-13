add9268d89f285f9c0ead66da9b4a999bf7adace
divdav06 <moustapha.khalifaa@gmail.com>
2026-04-12 20:17:09 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,10 @@
-export const metadata = {
-  title: "Selma App",
-  description: "Site de statistiques sportives",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
